540455daf12918ecf6f897a6857bc5a599d0cb2b
giospes <giovannijacopo.spes@gmail.com>
2023-03-26 21:14:57 +0200

    select done

---
+++ js/script.js
@@ -0,0 +1,157 @@
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    }
+];
+
+printAnimals()
+printVegetables()
+printUsers()
+
+const selectEl = document.getElementById("type")
+selectEl.addEventListener("change", function(){
+    const selectedValue = selectEl.value;
+    switch(selectedValue){
+        case 'User' :
+            printUsers()
+            console.log("User")
+            break;
+        case'Animals':
+            printAnimals()
+            console.log("Anim")
+            break;
+        case 'Vegetables':
+            printVegetables()
+            console.log("Veg")
+            break;
+        default:
+            printAnimals()
+            printVegetables()
+            printUsers()
+            console.log("User")
+    
+    }
+
+
+})
+
+function printAnimals(){
+
+}
+function printVegetables(){
+
+}
+function printUsers(){
+
+}
+
+
