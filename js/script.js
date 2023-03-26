85ad8e81038d7363c049dc79ecfa5954c79c5bf9
giospes <giovannijacopo.spes@gmail.com>
2023-03-26 22:13:49 +0200

    done

--- js/script.js
+++ js/script.js
@@ -152,25 +152,68 @@ function printAnimals(){
     let cardBlocks = [];
     icons.forEach(icon => {
        if(icon.type == 'animal'){
+            const color = rdColor()
             const card = document.createElement('div')
             card.className = "card bg-info gs-card d-flex flex-column align-items-center py-3"
             card.innerHTML =
             ` 
-                <i class="fa-solid fa-house ${icon.prefix}${icon.family}  ${icon.prefix}${icon.name} fs-2 "></i>
+                <i class="${icon.prefix}${icon.family}  ${icon.prefix}${icon.name} fs-2 " style = "color: ${color}"></i>
                 <p id="i-name" class="text-uppercase m-0">${icon.name}</p>
             `;
             cardBlocks.push(card.outerHTML);
         } 
 
     });
-    cardContainer.innerHTML = cardBlocks.join('')
+    cardContainer.innerHTML += cardBlocks.join('')
     
 }
+
 function printVegetables(){
+    let cardBlocks = [];
+    icons.forEach(icon => {
+       if(icon.type == 'vegetable'){
+            const color = rdColor()
+            const card = document.createElement('div')
+            card.className = "card bg-info gs-card d-flex flex-column align-items-center py-3"
+            card.innerHTML =
+            ` 
+                <i class=" ${icon.prefix}${icon.family}  ${icon.prefix}${icon.name} fs-2 " style = "color: ${color}"></i>
+                <p id="i-name" class="text-uppercase m-0">${icon.name}</p>
+            `;
+            cardBlocks.push(card.outerHTML);
+        } 
 
+    });
+    cardContainer.innerHTML += cardBlocks.join('')
 }
+
 function printUsers(){
+    let cardBlocks = [];
+    icons.forEach(icon => {
+       if(icon.type == 'user'){
+            const color = rdColor()
+            const card = document.createElement('div')
+            card.className = "card bg-info gs-card d-flex flex-column align-items-center py-3"
+            card.innerHTML =
+            ` 
+                <i class=" ${icon.prefix}${icon.family}  ${icon.prefix}${icon.name} fs-2 " style = "color: ${color}"></i>
+                <p id="i-name" class="text-uppercase m-0">${icon.name}</p>
+            `;
+            cardBlocks.push(card.outerHTML);
+        } 
 
+    });
+    cardContainer.innerHTML += cardBlocks.join('')
 }
 
 
+
+function rdColor(){
+    let hexCode = '#';
+
+    const hexDigits = '0123456789ABCDEF';
+    for (let i = 0; i < 6; i++) {
+        hexCode += hexDigits[Math.floor(Math.random() * 16)];
+    }
+    return hexCode;
+}
